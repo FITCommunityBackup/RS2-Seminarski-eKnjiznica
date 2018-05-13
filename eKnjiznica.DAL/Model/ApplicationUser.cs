782ac346fe669af0e56f2a2db61f3f2390a983f6
Petar Lukic <petar.lukic@edu.fit.ba>
2018-05-13 10:23:54 +0200

    Application user model update

--- eKnjiznica.DAL/Model/ApplicationUser.cs
+++ eKnjiznica.DAL/Model/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace eKnjiznica.DAL.Model
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public bool IsActive { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
